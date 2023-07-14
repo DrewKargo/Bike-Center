0fbb795c56969a83ed007127dfa493fe3888c9f6
DrewKargo <sagittarius.drew@gmail.com>
2023-07-14 14:28:15 +0300

    add menu

--- js/script.js
+++ js/script.js
@@ -3,8 +3,17 @@
 const search = document.querySelector('.search__input'),
 		searchModal = document.querySelector('.search__modal'),
 		signIn = document.querySelector('.sign-in'),
-		icon = document.querySelector('.sign-in__icon');
-
+		icon = document.querySelector('.sign-in__icon'),
+		body = document.querySelector('.body'),
+		menu = document.querySelector('.menu'),
+		catalogContainer = document.querySelector('.menu__container'),
+		brands = document.querySelector('.menu__brands'),
+		catalogToggle = document.querySelector('.menu__tabs-selector-mobile_toggle'),
+		catalogButton = document.querySelector('#mobile-catalog-btn'),
+		brandsButton = document.querySelector('#mobile-brands-btn'),
+		burger = document.querySelector('.header__burger');
+		
+		
 
 	let showModal = false,
 		auth = false;
@@ -22,7 +31,7 @@ const search = document.querySelector('.search__input'),
 	 
 		if ( ! withinBoundaries ) {
 			showModal = false;
-			searchModal.style.display = 'none'; // скрываем элемент т к клик был за его пределами
+			searchModal.style.display = 'none';
 		}
 	})
 
@@ -37,3 +46,95 @@ const search = document.querySelector('.search__input'),
 			auth = false;
 		}
 	})
+
+	let showMenu = false;
+
+	burger.addEventListener('click', () => {
+		let categories = document.querySelectorAll('.menu__categories_item');
+		categories.forEach(el => el.classList.remove('menu__categories_item-active'));
+		categories[0].classList.add('menu__categories_item-active');
+		
+		if(showMenu == false){
+			body.style.overflow = "hidden";
+			menu.classList.add('menu__open');
+			burger.classList.add('header__burger_close');
+
+			if(window.innerWidth > 767){
+				let firstCategory = document.querySelector("#Bicycles"),
+					tabcontent = document.querySelectorAll(".menu__subcategories");
+
+				for (let i = 0; i < tabcontent.length; i++) {
+		 			tabcontent[i].style.display = "none";
+				}
+				
+				firstCategory.style.display = "block";
+		
+				
+				const msnry = new Masonry( firstCategory,{
+					itemSelector: '.menu__subcategories_item',
+					columnWidth: 313,
+					gutter: 100
+				})
+			}
+
+			return showMenu = true;
+		} else {
+			body.style.overflow = "";
+			menu.classList.remove('menu__open');
+			burger.classList.remove('header__burger_close');
+
+			return showMenu = false;
+		}
+		
+
+	})
+	
+	let toggleInCatalogPosition = true;
+
+	brandsButton.addEventListener('click', () => {
+		if(toggleInCatalogPosition == true){
+			catalogToggle.classList.add('menu__tabs-selector-mobile_toggle-to-brands');
+			catalogContainer.style.display = "none";
+			brands.style.display = "block";
+			return toggleInCatalogPosition = false;
+		}
+	})
+	catalogButton.addEventListener('click', () => {
+		if(toggleInCatalogPosition == false){
+			catalogToggle.classList.remove('menu__tabs-selector-mobile_toggle-to-brands');
+			catalogContainer.style.display = "flex";
+			brands.style.display = "none";
+			return toggleInCatalogPosition = true;
+		}
+	})
+
+	
+
+	function openSubcategory(event, categoryName) {
+		
+		if(window.innerWidth > 767){
+			
+			let categories = document.querySelectorAll('.menu__categories_item'),
+				currentCategory = document.getElementById(categoryName), 
+				tabcontent = document.querySelectorAll(".menu__subcategories");
+			
+			for (let i = 0; i < tabcontent.length; i++) {
+		 		tabcontent[i].style.display = "none";
+			}
+
+			currentCategory.style.display = "block";
+			
+			categories.forEach(el => el.classList.remove('menu__categories_item-active'));
+			event.currentTarget.classList.add('menu__categories_item-active');
+			
+			
+			
+			const msnry = new Masonry( currentCategory,{
+				itemSelector: '.menu__subcategories_item',
+				columnWidth: 313,
+				gutter: 100
+			})
+		}
+	  
+	}
+	
